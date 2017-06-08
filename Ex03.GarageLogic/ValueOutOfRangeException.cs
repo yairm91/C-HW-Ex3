36ab3b1395e5f58978ea37f8f6ff0ca07bb83837
sahar konortov <saharkonortov@gmail.com>
2017-06-09 00:43:29 +0300

    addedValueOutOfRangeException and the loop to choose function to operate

---
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class ValueOutOfRangeException : Exception
+    {
+        private float m_MaxValue;
+        private float m_MinValue;
+
+        internal float MinValue
+        {
+            get { return m_MinValue; }
+        }
+
+        internal float MaxValue
+        {
+            get { return m_MaxValue; }
+        }
+
+        public ValueOutOfRangeException(int i_MinimumValue, int i_MaximumValue)
+            : base(string.Format(
+                "the input is not in the range between {0} to {1}",
+                i_MinimumValue,
+                i_MaximumValue))
+        {
+        }
+    }
+
+}
